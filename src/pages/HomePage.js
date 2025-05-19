8c457e127aa2b6c331fb772316637755bc665a53
YassinT99 <421184@student.fontys.nl>
2025-05-19 16:04:48 +0200

    Added global state code, router mapping and pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the to do list</h1>
+      <p></p>
+    </div>
+  );
+}
---
+++ src/pages/TodoPage.js
@@ -0,0 +1,8 @@
+export default function TodoPage() {
+  return (
+    <div>
+      <h1>Your Todos</h1>
+      {/* todo list component will go here */}
+    </div>
+  );
+}
